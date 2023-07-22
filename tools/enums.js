c10a588acbfc0f56e9a1ea11955510101816a64a
wisnudm13 <wisnudentadio@gmail.com>
2023-07-22 15:15:16 +0700

    add enum

---
+++ tools/enums.js
@@ -0,0 +1,14 @@
+const facilityStatus = {
+    ACTIVE: "ACTIVE",
+    INACTIVE: "INACTIVE"
+}
+
+const facilityType = {
+    SPORT: "SPORT",
+    INN: "INN"
+}
+
+module.exports = {
+    facilityType,
+    facilityStatus
+}
